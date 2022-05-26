96c0ee145130d6c820372758f7ee0c8083d7f497
Parka015 <PRM00@protonmail.com>
2022-05-27 01:26:47 +0200

    Primera implementación

---
+++ app/src/main/java/com/example/avimo/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.avimo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+    private EditText editText;
+    private ImageView micButton;
+    private ArrayList<String> data;
+    private TextView textView;
+
+    private TextToSpeech tts;
+
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        editText = findViewById(R.id.text);
+        micButton = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+                textView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {}
+
+            @Override
+            public void onRmsChanged(float v) {}
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {}
+
+            @Override
+            public void onEndOfSpeech() {}
+
+            @Override
+            public void onError(int i) {
+                tts.speak(getString(R.string.error_escucha).trim(), TextToSpeech.QUEUE_ADD, null);
+                textView.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                editText.setText(data.get(0));
+                textView.setVisibility(View.INVISIBLE);
+                main();
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {}
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {}
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    micButton.setImageResource(R.drawable.microfono);
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+
+                return false;
+            }
+        });
+
+        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status)
+            {
+                tts.setLanguage(Locale.ROOT);
+            }
+        });
+
+        tts.setPitch(.8f);
+        tts.setSpeechRate(.8f);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void main(){
+
+
+        tts.speak("Tu has dicho, "+data.toString().trim(), TextToSpeech.QUEUE_ADD, null);
+        editText.setText("Tu has dicho, "+data.toString());
+
+    }
+}
--- docs/Planificación del proyecto/README.md
+++ docs/Planificación del proyecto/README.md
@@ -1,23 +1,30 @@
 ## Tipo de usuario:
 La aplicación está pensada para ser usada por personas activas de cualquier edad que busquen cierta ayuda de gestión y organización del dia a dia.
 
-## Milestones [MX] :
- - **[M0] Estructura básica del AV (externo)** : Contendrá el código previo necesario antes de introducir cualquier funcionalidad.
- - **[M1] Paquete del Calendario (externo)** : En esta entrega se proporcionará al asistente la funcionalidad de la gestión de los eventos en el calendario.
 
-## Historias de usuario [HUX]:
+## Función del calendario:
 
-### HUs asociadas al Milestone 0 (Estructura básica del AV):
+El objeto principal del calendario son los eventos, estos pueden ser de distintos tipos (eventos, tareas, recordatorios...) y tendrán asociadas unas etiquetas para permitir agrupar varios eventos con similar temática. Es posible que un evento tenga más de 1 etiquetas o ninguna.
 
-- **[HU5] Voz para el asistente** : Como usuario, quiero que el asistente pueda comunicarse conmigo por voz.
-- **[HU7] Activación por voz del AV** : Como usuario quiero que el AV me preste atención cuando diga alguna palabra clave.
+### Funcionalidades:
 
-### HUs asociadas al Milestone 1 (Paquete del Calendario):
+- **[F0] Interacción por voz con el asistente** :  El asistente se comunicará por voz con el usuario para ejecutar cualquier funcionalidad.
+- **[F1] Activación por voz del AV** :  El AV estará en modo espera en todo momento pero para acceder a cualquier funcionalidad previamente habrá que decir un código por voz para que este interprete nuestra petición.
+- **[F2] Notificación de los eventos** :  El asistente notificará o no (a elección del usuario) los eventos cercanos siguiendo unas opciones predeterminadas (modo de aviso, fecha y hora), pero estas se podrán cambiar. El modo de aviso de un evento puede ser, por medio de una notificación o por voz.
+- **[F3] Sincronización con Google Calendar** :  La aplicación hará uso de la API de Google Calendar para gestionar los eventos.
 
-- **[HU1] Crear evento calendario** : Como usuario quiero poder crear un evento para que el asistente pueda recordármelo.
-- **[HU2] Borrar evento calendario** : Como usuario quiero poder eliminar un evento que haya creado.
-- **[HU3] Modificar evento calendario** : Como usuario quiero poder cambiar o actualizar un evento que haya creado.
-- **[HU4] Buscar hueco en calendario** : Como usuario quiero que el asistente sea capaz de buscar un hueco en mi calendario para añadir un nuevo evento o actividad.
-- **[HU6] Notificación de los eventos** : Como usuario, quiero que el asistente me avise del comienzo de un evento por medio de una notificación.
+- **[F4] Gestionar etiquetas** : Se podrán crear, borrar y modificar etiquetas. 
+
+- **[F5] Gestionar eventos** : Se podrán crear, borrar y modificar eventos, tanto el crear como el modificar permitirán asociar etiquetas al evento en cuestión, además habrá 2 maneras de crear un evento:
+  - Una manera sencilla donde solo se pida la fecha, el nombre y el tipo de evento (eventos, tareas, recordatorios...), dejando todos los demás campos con unos valores predeterminados.
+  - Una manera avanzada donde se permita personalizar todos los campos anteriores, además de configurar las notificaciónes y las etiquetas del evento.
+- **[F6] Listar eventos por fecha** : Se podrá preguntar que eventos hay un dia en concreto o un tramo de tiempo (varios dias consecutivos o un tramo horario de un día en concreto). También esta búsqueda se podrá filtrar por etiquetas o por el tipo de evento.
+- **[F7] Consultar fecha de evento por nombre** : Se podrá pedir al AV que recuerde la fecha de un evento por el nombre del evento.
+
+- **[F8] Buscar hueco en calendario** :  El AV será capaz de buscar un hueco en el calendario para añadir un nuevo evento o actividad.
+
+- **[F9] Comando de ayuda (opcional)** :  El AV dispondrá de una sección de preguntas frecuentes, orientada a enseñar sobre el uso básico de la aplicación. Se le podrá decir palabras clave (etiquetas, crear, recordatorios...) para que busque preguntas asociadas a dichas palabras. Posteriormente informará al usuario de las preguntas encontradas por si alguna se corresponde con la duda que tenía.
+
+
 
 
