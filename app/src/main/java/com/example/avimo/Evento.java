6ff422a78656ba27cea42e64875a9e3492c87af0
Parka015 <PRM00@protonmail.com>
2022-06-11 18:55:40 +0200

    Añadida excepción en crear evento

--- app/src/main/java/com/example/avimo/Evento.java
+++ app/src/main/java/com/example/avimo/Evento.java
@@ -17,29 +17,35 @@ public class Evento extends AppCompatActivity {
                                String tags, int all_day, int has_alarm){
 
 
+        try {
+            final ContentValues event = new ContentValues();
+            event.put(CalendarContract.Events.CALENDAR_ID, 3);
+            event.put(CalendarContract.Events.TITLE, title);
+            event.put(CalendarContract.Events.DESCRIPTION, tags);
+            event.put(CalendarContract.Events.EVENT_LOCATION, loc);
+            event.put(CalendarContract.Events.DTSTART, startMillis);//startTimeMillis
+            event.put(CalendarContract.Events.DTEND, endMillis);//endTimeMillis
+            event.put(CalendarContract.Events.ALL_DAY, all_day);   // 0 for false, 1 for true
+            event.put(CalendarContract.Events.HAS_ALARM, has_alarm); // 0 for false, 1 for true
+            String timeZone = TimeZone.getDefault().getID();
+            event.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone);
+            Uri baseUri;
 
-        final ContentValues event = new ContentValues();
-        event.put(CalendarContract.Events.CALENDAR_ID, 3);
-        event.put(CalendarContract.Events.TITLE, title);
-        event.put(CalendarContract.Events.DESCRIPTION, tags);
-        event.put(CalendarContract.Events.EVENT_LOCATION, loc);
-        event.put(CalendarContract.Events.DTSTART, startMillis);//startTimeMillis
-        event.put(CalendarContract.Events.DTEND, endMillis);//endTimeMillis
-        event.put(CalendarContract.Events.ALL_DAY, all_day);   // 0 for false, 1 for true
-        event.put(CalendarContract.Events.HAS_ALARM, has_alarm); // 0 for false, 1 for true
-        String timeZone = TimeZone.getDefault().getID();
-        event.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone);
-        Uri baseUri;
+            if (Build.VERSION.SDK_INT >= 8) {
+                baseUri = Uri.parse("content://com.android.calendar/events");
+            } else {
+                baseUri = Uri.parse("content://calendar/events");
+            }
 
-        if (Build.VERSION.SDK_INT >= 8) {
-            baseUri = Uri.parse("content://com.android.calendar/events");
-        } else {
-            baseUri = Uri.parse("content://calendar/events");
+            getApplicationContext().getContentResolver().insert(baseUri, event);
+
+            //Toast.makeText(getApplicationContext(), "Event Created", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e){
+            return false;
         }
 
-        getApplicationContext().getContentResolver().insert(baseUri, event);
-
-        //Toast.makeText(getApplicationContext(), "Event Created", Toast.LENGTH_SHORT).show();
+        return true;
 
     }
 }
